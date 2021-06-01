00bba69b3ae300209ec6f4f4fd0cc3564819feb9
ErickCristhian <30393905+ErickCristhian@users.noreply.github.com>
2021-06-01 13:51:14 -0300

    Implementado alguns testes simples de renderização

--- vacinacao-frontend/src/__tests__/pages/Vacina.spec.js
+++ vacinacao-frontend/src/__tests__/pages/Vacina.spec.js
@@ -4,14 +4,38 @@ import "@testing-library/jest-dom/extend-expect";
 
 
 import Vacina from "../../pages/Vacina/Create";
-import Header from "../../components/Header";
 
 describe("Testing Vacina.js", () => {
   // Deve ser possível exibir o elemento h1 na página
   it("should be able to show the h1 element", () => {
     render(<Vacina />);
-    const h1Element = screen.getByText(/cadastrar nova vacina/i);
+    let h1Element = screen.getByText(/cadastrar nova vacina/i);
 
     expect(h1Element).toBeInTheDocument();
   });
+  it("should be able to show the Label Total de doses element", () => {
+    render(<Vacina />);
+    let Element = screen.getByText(/total de doses/i);
+
+    expect(Element).toBeInTheDocument();
+  });
+  it("should be able to show the Label Nome element", () => {
+    render(<Vacina />);
+    let Element = screen.getByText(/nome/i);
+
+    expect(Element).toBeInTheDocument();
+  });
+  it("should be able to show the Label Intervalo element", () => {
+    render(<Vacina />);
+    let Element = screen.getByText(/intervalo/i);
+
+    expect(Element).toBeInTheDocument();
+  });
+  it("should be able to show the Button Cadastrar element", () => {
+    render(<Vacina />);
+    let Element = screen.getByRole('button');
+
+    expect(Element).toBeInTheDocument();
+  });
+  
 });
